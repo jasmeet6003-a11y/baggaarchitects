0c11b44004089e684ef04384b0820e92f717f068
jasmeet6003-a11y <jasmeet6003@gmail.com>
2026-06-17 14:36:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 document.addEventListener("DOMContentLoaded", function () {
-
-    const nav = document.querySelector(".nav");
     const hamburger = document.querySelector(".nav__hamburger");
+    const nav = document.querySelector(".nav");
 
     if (hamburger && nav) {
         hamburger.addEventListener("click", function () {
             nav.classList.toggle("open");
         });
     }
-
 });
